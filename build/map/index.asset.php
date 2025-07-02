8c424db4f331ff2a9c8e6b54f66caf8d6a6dc751
Alexander Bigga <alexander@bigga.de>
2025-07-02 23:25:47 +0200

    Add height settings to block

--- build/map/index.asset.php
+++ build/map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'cc2df5dc93c37c60d5f8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '413685e002b527d28957');
