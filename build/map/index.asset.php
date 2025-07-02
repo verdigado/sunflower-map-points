e08f6168100f64a24aaa5ccf38ecc6f6fe036f1e
Alexander Bigga <alexander@bigga.de>
2025-07-02 23:44:07 +0200

    Fix height and rise version number 1.2.1

--- build/map/index.asset.php
+++ build/map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '413685e002b527d28957');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'e919c931798f20c3d8af');
--- build/map/index.js
+++ build/map/index.js
@@ -74,9 +74,7 @@ function Edit({
           children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
             id: "map",
             style: {
-              height: {
-                height
-              } + 'px'
+              height: `${parseInt(height, 10) || 400}px`
             }
           })
         })
