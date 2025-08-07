ad9e0ceba76c49dfd5b440bdb1eb9a654b8a9b71
Alexander Bigga <alexander.bigga@verdigado.com>
2025-08-07 17:59:29 +0200

    Avoid duplicate entries

--- build/map/index.asset.php
+++ build/map/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6f535f5a4daf83999475');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a4d942131e103d5e2bfc');
