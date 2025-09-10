cdb69f666d048e8a2156b70c70d2c3e531af28aa
Alexander Bigga <alexander.bigga@verdigado.com>
2025-09-10 23:18:07 +0200

    Make independant of sunflower theme - bootstrap based theme recommended (#18)
    * Make independant of sunflower theme - bootstrap based theme recommended
    
    * Remove build directory from repository
    
    * Lint javascript

--- build/map/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a4d942131e103d5e2bfc');
--- build/map/map-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'bf0e7a23121f38f20fa3');
