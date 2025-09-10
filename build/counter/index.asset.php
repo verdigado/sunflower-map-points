988fae8b5affbdb03b06f263fef16b1191ea54ef
Alexander Bigga <alexander@bigga.de>
2025-09-10 23:11:00 +0200

    Remove build directory from repository

--- build/counter/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'c11a12dc64ec2f57b23e');
--- build/map/map-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '75bc789a48b8e2b0fd26');
