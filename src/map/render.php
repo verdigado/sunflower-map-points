4b89a84644a0384c81637098db5645e9e49a8105
Alexander Bigga <alexander@bigga.de>
2025-11-13 21:14:00 +0100

    Add some field descriptions and translations

--- src/map/render.php
+++ src/map/render.php
@@ -24,8 +24,8 @@ if ( isset( $attributes['lat'] ) && ! empty( $attributes['lat'] ) ) {
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title">Hinweis geben</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Schließen"></button>
+				<h5 class="modal-title"><?php esc_attr_e( 'Report an issue', 'sunflower-map-points' ); ?></h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="<?php esc_attr_e( 'Close', 'sunflower-map-points' ); ?>"></button>
 			</div>
 			<div class="modal-body">
 
@@ -39,47 +39,79 @@ if ( isset( $attributes['lat'] ) && ! empty( $attributes['lat'] ) ) {
 					<input type="hidden" id="form-lat" name="lat">
 					<input type="hidden" id="form-lng" name="lng">
 					<input type="hidden" name="mailTo" value="
-					<?php
-					echo esc_attr(
-						strrev(
-				base64_encode( $sunflower_map_points_mailto ) //phpcs:ignore
-						)
-					)
-					?>
-		">
-
+						<?php
+							echo esc_attr(
+								strrev(
+									base64_encode( $sunflower_map_points_mailto ) //phpcs:ignore
+								)
+							)
+							?>
+					">
 					<div class="mb-3">
-						<label for="name" class="form-label">Name (*)</label>
-						<input type="text" name="name" id="name" class="form-control" required>
+						<label for="name" class="form-label"><?php esc_attr_e( 'Name', 'sunflower-map-points' ); ?> (*)</label>
+						<input type="text" name="name" id="name" class="form-control" required aria-describedby="nameHelp">
+						<div id="nameHelp" class="form-text text-muted">
+							<?php
+								/* translators: This string may contain <strong> tags. */
+								echo wp_kses(
+									__( 'This information will <strong>not be published</strong>.', 'sunflower-map-points' ),
+									array(
+										'strong' => array(),
+									)
+								);
+								?>
+						</div>
 					</div>
 
 					<div class="mb-3">
-						<label for="email" class="form-label">Email (*)</label>
-						<input type="email" name="email" id="email" class="form-control" required>
+						<label for="email" class="form-label"><?php esc_attr_e( 'E-Mail', 'sunflower-map-points' ); ?> (*)</label>
+						<input type="email" name="email" id="email" class="form-control" required aria-describedby="emailHelp">
+						<div id="emailHelp" class="form-text text-muted">
+							<?php
+								/* translators: This string may contain <strong> tags. */
+								echo wp_kses(
+									__( 'Used only for follow-up questions and feedback. Will <strong>not be published</strong>.', 'sunflower-map-points' ),
+									array(
+										'strong' => array(),
+									)
+								);
+								?>
+						</div>
 					</div>
 
 					<div class="mb-3">
-						<label for="phone" class="form-label">Telefon</label>
-						<input type="tel" name="phone" id="phone" class="form-control">
+						<label for="phone" class="form-label"><?php esc_attr_e( 'Phone', 'sunflower-map-points' ); ?></label>
+						<input type="tel" name="phone" id="phone" class="form-control" aria-describedby="phoneHelp">
+						<div id="phoneHelp" class="form-text text-muted">
+							<?php
+								/* translators: This string may contain <strong> tags. */
+								echo wp_kses(
+									__( 'Optional, helps with inquiries. Will <strong>not be published</strong>.', 'sunflower-map-points' ),
+									array(
+										'strong' => array(),
+									)
+								);
+								?>
+						</div>
 					</div>
 
 					<div class="mb-3">
-						<label for="topic" class="form-label">Thema (*)</label>
+						<label for="topic" class="form-label"><?php esc_attr_e( 'Topic', 'sunflower-map-points' ); ?> (*)</label>
 						<select id="topic" name="topic" class="form-select">
 							<?php foreach ( $attributes['topics'] as $sunflower_map_points_topics ) : ?>
 								<option value="<?php echo esc_attr( trim( $sunflower_map_points_topics ) ); ?>">
-									<i className="fa-solid fa-bicycle"></i> <?php echo esc_html( trim( $sunflower_map_points_topics ) ); ?>
+									<?php echo esc_html( trim( $sunflower_map_points_topics ) ); ?>
 								</option>
 							<?php endforeach; ?>
 						</select>
 					</div>
 
 					<div class="mb-3">
-						<label for="message" class="form-label">Hinweis (*)</label>
+						<label for="message" class="form-label"><?php esc_attr_e( 'Hint', 'sunflower-map-points' ); ?> (*)</label>
 						<textarea name="message" id="message" class="form-control" rows="10" required></textarea>
 					</div>
 
-					<button type="submit" class="btn btn-primary w-100">Senden</button>
+					<button type="submit" class="btn btn-primary w-100"><?php esc_attr_e( 'Send', 'sunflower-map-points' ); ?></button>
 				</form>
 				<div id="form-message" class="alert d-none mt-3" role="alert"></div>
 			</div>
